fefb39df10778151141dda359066139578591b8d
Kiyotaka Abe <k-abe@s-giken.co.jp>
2016-05-17 10:03:24 +0900

    一区切り

---
+++ C4SampleConsoleApplication/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//dynamic型
+			try {
+				DynamicSample();
+			}
+			catch { }
+
+			//************************************************
+			{ 
+			var e1 = new Employee("abe1") { Name = "abe2" };
+			var e2 = new Employee(name: "ok");
+			var e3 = new Employee("kiyo", age: 12);
+
+			Console.WriteLine($"e1.Name={e1.Name}");//abe2
+			}
+			//************************************************
+			{ 
+			string[] a = { "a", "b", "c" };
+			foreach (var n in a.Select((s, i) => new { i, s }))
+				Console.WriteLine($"{n.s}さんは、{n.i + 1}位です。");
+			}
+			//************************************************
+			{ 
+			string[] a = { "a", "b", null, "d" };
+			foreach (var n in a.Where(c => c != null).Select((s, i) => new { i, s }))
+				Console.WriteLine($"{n.s}さんは、{n.i + 1}位です。");
+			}
+			//************************************************
+			{
+				string str = "123a";
+				int result;
+				result = int.TryParse(str, out result) ? result : -1;
+				Console.WriteLine($"{result}");//-1
+				result = -2;
+				int.TryParse(str, out result);
+				Console.WriteLine($"{result}");//0
+			}
+			//************************************************
+			{
+				有効無効 a;
+				Enum.TryParse<有効無効>("はてな", out a);
+				Console.WriteLine($"{a}");	//無効
+				Enum.TryParse<有効無効>("有効", out a);
+				Console.WriteLine($"{a}");	//有効
+
+			}
+			//************************************************
+			{
+				string a = null;
+				Console.WriteLine(a ?? "(null)");	//(null)
+				a = "a";
+				Console.WriteLine(a ?? "(null)");	//a
+			}
+			//************************************************
+			{
+				var ex = new Example() { A = "{0} {1}", B = "bbb", C = "cccc" };
+				Console.WriteLine($"{ex.A}", $"{ex.B}", $"{ex.C}");
+			}
+			//************************************************
+			{ 
+				Action cut = () => { };
+				string str = "abc";
+				if (str.Length > 0)
+				{
+					cut = () => str = str.Remove(str.Length - 1, 1);
+				}
+				cut();
+				Console.WriteLine($"{str}");
+			}
+			//************************************************
+			{
+				Action<string> action = s => { };
+				Greeting(s => { Console.WriteLine($"{s}さん"); });
+				Greeting(s => { Console.WriteLine($"{s}くん"); });
+			}
+
+
+			Console.ReadLine();
+		}
+
+		static void Greeting(Action<string> action)
+		{
+			Console.WriteLine("start");
+			action("hogehoge");
+			Console.WriteLine("end");
+		}
+
+		class Example { internal string A, B, C; }
+
+		enum 有効無効
+		{
+			有効 = 1,
+			無効 = 0
+		}
+
+		class Employee
+		{
+			public Employee(string name, int age = 0, string bikou = "")
+			{
+				Name = name;
+			}
+
+			public string Name { get; set; }
+		}
+
+		static void DynamicSample()
+		{
+			dynamic dyn = 1;
+			object obj = 1;
+
+			Console.WriteLine(dyn.GetType());
+			Console.WriteLine(obj.GetType());
+
+			dyn.HogeHoge();
+			//obj.HogeHoge();
+			/*
+  Microsoft.CSharp.RuntimeBinder.RuntimeBinderException はハンドルされませんでした。
+  HResult=-2146233088
+  Message='int' に 'HogeHoge' の定義がありません
+  Source=Anonymously Hosted DynamicMethods Assembly
+  StackTrace:
+       場所 CallSite.Target(Closure , CallSite , Object )
+       場所 System.Dynamic.UpdateDelegates.UpdateAndExecuteVoid1[T0](CallSite site, T0 arg0)
+       場所 ConsoleApplication.Program.DynamicSample() 場所 D:\dev\sample\20160426_C#4.0\ConsoleApplication\ConsoleApplication\Program.cs:行 30
+       場所 ConsoleApplication.Program.Main(String[] args) 場所 D:\dev\sample\20160426_C#4.0\ConsoleApplication\ConsoleApplication\Program.cs:行 14
+       場所 System.AppDomain._nExecuteAssembly(RuntimeAssembly assembly, String[] args)
+       場所 System.AppDomain.ExecuteAssembly(String assemblyFile, Evidence assemblySecurity, String[] args)
+       場所 Microsoft.VisualStudio.HostingProcess.HostProc.RunUsersAssembly()
+       場所 System.Threading.ThreadHelper.ThreadStart_Context(Object state)
+       場所 System.Threading.ExecutionContext.RunInternal(ExecutionContext executionContext, ContextCallback callback, Object state, Boolean preserveSyncCtx)
+       場所 System.Threading.ExecutionContext.Run(ExecutionContext executionContext, ContextCallback callback, Object state, Boolean preserveSyncCtx)
+       場所 System.Threading.ExecutionContext.Run(ExecutionContext executionContext, ContextCallback callback, Object state)
+       場所 System.Threading.ThreadHelper.ThreadStart()
+  InnerException: 
+			 */
+		}
+
+
+	}
+}
